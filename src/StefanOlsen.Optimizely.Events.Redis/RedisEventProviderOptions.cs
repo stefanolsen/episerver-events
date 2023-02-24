75e60464b9c072b64a5a9c151412b31b6f6fb043
Stefan Holm Olsen <stefan@stefanolsen.com>
2023-02-24 22:38:17 +0300

    Add Redis PubSub event provider.

---
+++ src/StefanOlsen.Optimizely.Events.Redis/RedisEventProviderOptions.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.ServiceLocation;
+
+namespace StefanOlsen.Optimizely.Events.Redis
+{
+    [Options(ConfigurationSection = "Cms")]
+    public class RedisEventProviderOptions
+    {
+        public string ConnectionString { get; init; }
+        public string PubSubChannel { get; init; }
+    }
+}
